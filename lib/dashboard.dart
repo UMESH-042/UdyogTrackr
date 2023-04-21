a444ae1e474240a6baa8e7ac71c0960fc9dfe9c0
UMESH-042 <202151042@iiitvadodara.ac.in>
2023-04-22 01:29:45 +0530

    Third Commit

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -37,61 +37,212 @@ class _dashboardState extends State<dashboard> {
         ),
       ),
 
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+  drawer: Drawer(
+  child: ListView(
+    padding: EdgeInsets.zero,
+    children: [
+      DrawerHeader(
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[900],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blueGrey[900],
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/background.jpg'),
             ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
+            SizedBox(height: 5),
+            Text(
+              'Business Name',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
       ),
-      body: Container(
-        child:
-      ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemExtent: 200,
-        itemCount: imageUrls.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            alignment: Alignment.topCenter,
-            margin: EdgeInsets.all(8.0),
-            // padding: EdgeInsets.all(10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.network(
-               
-                imageUrls[index],
-                width: 500,
-                height: 500,
-                // fit: BoxFit.cover,
-              ),
-            ),
-          );
+      ListTile(
+        leading: Icon(Icons.info),
+        title: Text('About Us'),
+        onTap: () {
+          // handle about us tap
+          Navigator.pop(context);
         },
       ),
+      ListTile(
+        leading: Icon(Icons.contacts),
+        title: Text('Contacts'),
+        onTap: () {
+          // handle contacts tap
+          Navigator.pop(context);
+        },
+      ),
+      Divider(),
+      ListTile(
+        leading: Icon(Icons.settings),
+        title: Text('Settings'),
+        onTap: () {
+          // handle settings tap
+          Navigator.pop(context);
+        },
+      ),
+    ],
+  ),
+),
+
+
+ body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'About Us',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centurie It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Our Works',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/background.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/background.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/background.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Owners',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage('assets/background.jpg'),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Owner 1',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text('Owner title'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage('assets/background.jpg'),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Owner 2',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text('Owner title'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Contact Us',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.phone),
+                    title: Text('Phone'),
+                    subtitle: Text('+1234567890'),
+                    onTap: () {
+                      // handle phone tap
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text('Email'),
+                    subtitle: Text('info@business.com'),
+                    onTap: () {
+                      // handle email tap
+                    },
+                                      ),
+                  ListTile(
+                    leading: Icon(Icons.location_on),
+                    title: Text('Address'),
+                    subtitle: Text('123 Main Street, City, State Zip'),
+                    onTap: () {
+                      // handle address tap
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
